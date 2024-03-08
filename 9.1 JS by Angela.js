f1178a84385a0c8390ea190dbedb0c5f3ab7ad73
Shubham <152170670+shubhamkfa@users.noreply.github.com>
2024-03-08 23:03:06 +0530

    Add files via upload

---
+++ 9.1 JS by Angela.js
@@ -0,0 +1,43 @@
+// alert ("Hello");
+// alert("Say Hello");
+// typeof(1);
+// typeof("hello")
+// prompt("Enter your Mobile Number ");
+
+
+// var myName=("Shubham");
+// alert (myName);
+// var yourName=prompt("What is Your Wifes Name?");
+// alert("My name is "+ myName +" and my wifes name is "+yourName+"!");
+// console.log(myName);
+
+
+// var gameLevel=1;
+// gameLevel=2;
+// gameLevel=3;
+// alert("Your Game Level is "+ gameLevel);
+
+
+
+var a = "3";
+var b = "8";
+
+/***********Do not change the code above 👆*******/
+//Write your code on lines 7 - 9:
+// var c=a;
+// a=b;
+// b=c;
+
+
+
+// below code will work when number declared in number form not in string form.
+// var c=a+b;  
+// a=c-a;
+// b=c-b;
+
+
+
+/***********Do not change the code below 👇*******/
+
+console.log("a is " + a);
+console.log("b is " + b);
